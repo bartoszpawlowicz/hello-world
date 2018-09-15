a1b97592693cfeb052afe4353b990914428093d8
bartoszpawlowicz <pawlowicz.bartosz@gmail.com>
2018-09-15 12:33:22 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 class Message:
+    """ This class handles a message
+    
+        At this moment only a content is handles cause I don't yet have an idea what else to add
+    """
     def __init__(self, content="Hello World"):
         self.content = content
 
